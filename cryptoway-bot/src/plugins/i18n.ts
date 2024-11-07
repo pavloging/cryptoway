02fd4e89bcaeb21dedc566e2ece9e61d4495e43f
Egor Pavlov <ep9821123@gmail.com>
2024-11-08 01:12:14 +0300

    fix: path

--- cryptoway-bot/src/plugins/i18n.ts
+++ cryptoway-bot/src/plugins/i18n.ts
@@ -1,9 +1,10 @@
 import { I18n } from "@grammyjs/i18n";
 import { ContextType } from "../types/index.js";
+import path from "path";
 
 export const i18n = new I18n<ContextType>({
     defaultLocale: "en",
-    directory: "../locales",
+    directory: path.resolve(process.cwd(), 'src/locales'),
 });
 
 export const i18nMiddleware = i18n.middleware()
