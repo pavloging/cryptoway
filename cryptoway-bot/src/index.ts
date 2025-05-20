125b13cb42da133ee856f78d5bbb28ba4727f9a7
Egor <ep9821123@gmail.com>
2025-05-20 17:12:40 +0300

    fix: error types

--- cryptoway-bot/src/index.ts
+++ cryptoway-bot/src/index.ts
@@ -1,21 +1,21 @@
-import "dotenv/config";
-import "./database/db.js";
+import 'dotenv/config';
+import './database/db.js';
 
-import { Bot, session } from "grammy";
-import { freeStorage } from "@grammyjs/storage-free";
-import { ContextType, SessionData } from "./types/index.js";
-import { setMyCommands } from "./untils/setMyCommands.js";
-import { composer as startCommand } from "./composers/commands/start.command.js";
-import { composer as helpCommand } from "./composers/commands/help.command.js";
-import { composer as settingsCommand } from "./composers/commands/settings.command.js";
-import { composer as coinsCommand } from "./composers/commands/coins.command.js";
-import { composer as startCallback } from "./composers/callbacks/start.callback.js";
-import { composer as settingsCallback } from "./composers/callbacks/settings.callback.js";
-import { composer as noneMessage } from "./composers/messages/none.messages.js";
-import { i18nMiddleware, limitMiddleware } from "./plugins/index.js";
-import { setCheckCoins } from "./untils/setCheckCoins.js";
+import { Bot, session } from 'grammy';
+import { freeStorage } from '@grammyjs/storage-free';
+import { ContextType, SessionData } from './types/index.js';
+import { setMyCommands } from './untils/setMyCommands.js';
+import { composer as startCommand } from './composers/commands/start.command.js';
+import { composer as helpCommand } from './composers/commands/help.command.js';
+import { composer as settingsCommand } from './composers/commands/settings.command.js';
+import { composer as coinsCommand } from './composers/commands/coins.command.js';
+import { composer as startCallback } from './composers/callbacks/start.callback.js';
+import { composer as settingsCallback } from './composers/callbacks/settings.callback.js';
+import { composer as noneMessage } from './composers/messages/none.messages.js';
+import { i18nMiddleware, limitMiddleware } from './plugins/index.js';
+import { setCheckCoins } from './untils/setCheckCoins.js';
 
-if (!process.env.TOKEN) throw new Error("invalided token");
+if (!process.env.TOKEN) throw new Error('invalided token');
 
 const bot = new Bot<ContextType>(process.env.TOKEN);
 
@@ -24,24 +24,31 @@ setMyCommands(bot);
 
 // Plugins
 bot.use(i18nMiddleware);
-bot.use(limitMiddleware)
+bot.use(limitMiddleware);
 
-bot.use(session({
-    initial: () => ({ type: "base", percent: 5 }),
-    storage: freeStorage<SessionData>(bot.token),
-  }));
+// For Error Types
+const storage = freeStorage<SessionData>(bot.token);
+bot.use(
+    session({
+        initial: () => ({ type: 'base', percent: 5 }),
+        storage: {
+            read: storage.read,
+            write: storage.write,
+            delete: storage.delete,
+        },
+    })
+);
 
 // Handlers
 bot.use(startCommand);
-bot.use(settingsCommand)
-bot.use(coinsCommand)
+bot.use(settingsCommand);
+bot.use(coinsCommand);
 bot.use(helpCommand);
-bot.use(startCallback)
-bot.use(settingsCallback)
-bot.use(noneMessage)
+bot.use(startCallback);
+bot.use(settingsCallback);
+bot.use(noneMessage);
 
-setCheckCoins(bot)
+setCheckCoins(bot);
 // Start
-bot.catch((error) => console.error(error))
+bot.catch((error) => console.error(error));
 bot.start();
-
